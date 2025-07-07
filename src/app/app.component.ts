230839b1b94043f0a0bab960b80342074a5df25a
AlouiOmar97 <omar.aloui@esprit.tn>
2025-07-07 20:11:53 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstapp';
+  title: string = '1CINFO1';
+  
+  hello(): void {
+   /* t
+    if(t){
+      var t
+    }*/
+    let list = [4, 5, 6];
+    for (let i in list) {
+    console.log(i);} 
+    for (let i of list) {
+    console.log(i);}
+  }
 }
