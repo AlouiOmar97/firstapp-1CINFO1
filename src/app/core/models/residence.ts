c0e471a6697ba1dca8f7e7aea8287d1543ecdb38
AlouiOmar97 <omar.aloui@esprit.tn>
2025-07-10 20:09:52 +0100

    s3

--- src/app/core/models/residence.ts
+++ src/app/core/models/residence.ts
@@ -4,4 +4,5 @@ name!: string;
 address!: string;
 image!: string;
 status!: string
+showAddress!: boolean;
 }
--- src/app/residence/residence.component.ts
+++ src/app/residence/residence.component.ts
@@ -9,14 +9,18 @@ import { Residence } from '../core/models/residence';
 export class ResidenceComponent {
 listResidences:Residence[]=[
 {id:1,"name": "El fel","address":"Borj Cedria",
-"image":"../../assets/images/R1.jpeg", status: "Disponible"},
+"image":"../../assets/images/R1.jpg", status: "Disponible", "showAddress": false},
 {id:2,"name": "El yasmine",
 "address":"Ezzahra","image":"../../assets/images/R2.jpg", status:
-"Disponible" },
+"Disponible", "showAddress": false },
 {id:3,"name": "El Arij",
 "address":"Rades","image":"../../assets/images/R3.jpg", status:
-"Vendu"},
+"Vendu", "showAddress": false},
 {id:4,"name": "El Anber","address":"inconnu",
-"image":"../../assets/images/R4.jpg", status: "En Construction"}
+"image":"../../assets/images/R4.jpg", status: "En Construction", "showAddress": true}
 ];
+
+showAddress(i: number){
+this.listResidences[i].showAddress= !this.listResidences[i].showAddress
+}
 }
